69bfe1bc15b5d163024ff14df2b555ec5ee7e27b
Genteure <genteure@gmail.com>
2018-12-23 09:36:08 +0800

    client 大体结构

--- BililiveStreamCrawler.Client/ClientMain.cs
+++ BililiveStreamCrawler.Client/ClientMain.cs
@@ -1,22 +1,32 @@
-﻿using System;
+﻿using BililiveStreamCrawler.Common;
+using System;
+using System.Collections.Generic;
 using System.Net.Security;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
 using WebSocketSharp;
 
 namespace BililiveStreamCrawler.Client
 {
     internal class ClientMain
     {
+        private static readonly List<StreamRoom> StreamRooms = new List<StreamRoom>();
+
+        private static readonly int MaxParallelTask = 3;
+
+        private static WebSocket WebSocket;
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TODO: parse config file
 
-            var ws = new WebSocket("wss://echo.websocket.org");
+            WebSocket = new WebSocket("wss://echo.websocket.org");
 
-            ws.SslConfiguration.CheckCertificateRevocation = false;
-            ws.SslConfiguration.EnabledSslProtocols = SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls;
-            ws.SslConfiguration.ServerCertificateValidationCallback =
+            WebSocket.SslConfiguration.CheckCertificateRevocation = false;
+            WebSocket.SslConfiguration.EnabledSslProtocols = SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls;
+            WebSocket.SslConfiguration.ServerCertificateValidationCallback =
                 (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
                 {
                     if (certificate is X509Certificate2 cert)
@@ -30,10 +40,78 @@ namespace BililiveStreamCrawler.Client
                     }
                 };
 
-            ws.Connect();
+            WebSocket.OnOpen += Ws_OnOpen;
+            WebSocket.OnMessage += Ws_OnMessage;
+            WebSocket.OnError += Ws_OnError;
+            WebSocket.OnClose += Ws_OnClose;
 
-            ws.Close();
+            WebSocket.Connect();
 
+
+            while (WebSocket.IsAlive)
+            {
+                Thread.Sleep(500);
+            }
+
+            try
+            {
+                WebSocket.Close();
+            }
+            catch (Exception) { }
+        }
+
+        private static void Ws_OnClose(object sender, CloseEventArgs e)
+        {
+
+        }
+
+        private static void Ws_OnError(object sender, ErrorEventArgs e)
+        {
+
+        }
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            StreamRoom streamRoom = null; // TODO: parse data from websocket
+
+            StreamRooms.Add(streamRoom);
+            Task.Run(() => StreamParser.Parse(streamRoom))
+                .ContinueWith((task) =>
+                {
+                    StreamRooms.Remove(streamRoom);
+                    if (!task.IsFaulted)
+                    {
+                        StreamMetadata data = task.Result;
+                        // ws.Send(data);
+                        // TODO: report success
+                    }
+                    else
+                    {
+                        string error = task.Exception.ToString();
+                        // ws.Send(error);
+                        // TODO: report error
+                    }
+                    TryRequestNewTask();
+                });
+            TryRequestNewTask();
+        }
+
+        private static void Ws_OnOpen(object sender, EventArgs e)
+        {
+            TryRequestNewTask();
+        }
+
+        private static void TryRequestNewTask()
+        {
+            if (StreamRooms.Count < MaxParallelTask)
+            {
+                RequestNewTask();
+            }
+        }
+
+        private static void RequestNewTask()
+        {
+            // TODO
         }
     }
 }
---
+++ BililiveStreamCrawler.Client/StreamParser.cs
@@ -0,0 +1,13 @@
+﻿using BililiveStreamCrawler.Common;
+
+namespace BililiveStreamCrawler.Client
+{
+    internal static class StreamParser
+    {
+        public static StreamMetadata Parse(StreamRoom room)
+        {
+            // TODO
+            return null;
+        }
+    }
+}
